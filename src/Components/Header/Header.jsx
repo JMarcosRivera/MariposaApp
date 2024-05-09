4fb6fd632d36b61c18481df3af8fb45ecc9d38bd
JMarcos <joanmarcosrh@gmail.com>
2024-05-08 20:05:12 -0400

    NavBar component added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,21 +3,75 @@ import './header.css';
 import MariposaIcon from "../../Resources/Icons/logo-transparente.jpeg";
 import { RiMenuLine } from "react-icons/ri";
 import ShoopingCart from "../ShoopingCart/ShoopingCart"
+import { useRef , useState} from 'react';
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { MdCancel } from "react-icons/md";
 
-function Header(){
+
+function Header() {
+
+    const ref = useRef(null);
+    const [open , setOpen] = useState(0);
+  
+
+    const handleNavScroll = () => {
+        let nav = ref.current;
+        if (nav.classList.contains('show')) {
+            nav.classList.remove('show');
+            setOpen(0);
+        }
+        else {
+            nav.classList.add('show');
+            setOpen(1);
+        }
+    }
 
     return (
-        <header className="header">
-            <div className="Menu">
-                <RiMenuLine className="menu-icon" />
-            </div>
-            
-            <div className="logo-container">
-                <img src={MariposaIcon} alt="Logo Mariposa" className="logo"/>
-            </div>
+        <>
+            <header className="header">
+                
+                <div className="Menu">
+                    
+                    <div className="menu-icon" onClick={handleNavScroll} >
+                        {
+                            open === 0 ? (<RiMenuLine/>) : (<MdCancel/>)
+                        }
+                    </div>
+                    
+                    
+                </div>
 
-            <ShoopingCart />
-        </header>
+                <div className="logo-container">
+                    <img src={MariposaIcon} alt="Logo Mariposa" className="logo" />
+                </div>
+
+                <ShoopingCart />
+            </header>
+            <ul className="menu-horizontal" ref={ref}>
+                <li className="navbar-li"> <a className="menu-item" href="#Inicio"> Inicio </a></li>
+                <li className="navbar-li"> <a className="menu-item" href="#Recomendados"> Recomendados </a></li>
+                <li className="navbar-li"> <a className="menu-item" href="#Menu"> Menu </a></li>
+                <li className="navbar-li"> <a className="menu-item" href="#"> Acerca de Nosotros </a></li>
+
+                <div className="social-container">
+                    <div className="social-item">
+                        <FaFacebookF className="social-icon"/>
+                    </div>
+                    <div className="social-item">
+                        <FaInstagram className="social-icon"/>
+                    </div>
+                    <div className="social-item" >
+                        <FaYoutube  className="social-icon"/>
+                    </div>
+
+                </div>
+            </ul>
+
+
+        </>
+
     );
 }
 
